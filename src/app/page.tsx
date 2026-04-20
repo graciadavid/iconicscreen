e58428bf7622c082777a563e6ddaaf4a66e26549
graciadavid <gracia.david@gmail.com>
2026-04-21 01:21:16 +0200

    screen micro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,10 +20,10 @@ screenStyle, setScreenStyle] = useState({top:0,left:0,width:0,height:0})
 
   const ORIG_W = 1344
   const ORIG_H = 768
-  const SCREEN_X1 = 475
+  const SCREEN_X1 = 476
   const SCREEN_Y1 = 115
   const SCREEN_X2 = 835
-  const SCREEN_Y2 = 485
+  const SCREEN_Y2 = 470
 
   function calcScreen() {
     const img = imgRef.current
